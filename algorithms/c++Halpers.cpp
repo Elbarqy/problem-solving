485b3ee6d95c999ab0cb1c6b39a353310d4b6c6e
Elbarqy <elbarqy.dev@gmail.com>
2022-10-29 07:14:04 +0200

    c++Halpers

---
+++ algorithms/c++Halpers.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+void inputRoutine()
+{
+    int Tc, a, b;
+    scanf("%d", &Tc);
+    while (Tc--)
+    {
+        scanf("%d %d", &a, &b);
+    }
+    while (*(scanf("%d %d"), &a, &b) != EOF)
+    {
+        printf("processing the %d and %d", &a, &b);
+    }
+}
